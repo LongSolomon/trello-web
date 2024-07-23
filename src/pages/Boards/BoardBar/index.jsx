418eb97dcb975f585a8fc1454b51c5743e2760cf
{Longsolomon} <thang0908441098@gmail.com>
2024-07-23 14:39:08 +0700

    Finish Board Bar

--- src/pages/Boards/BoardBar/index.jsx
+++ src/pages/Boards/BoardBar/index.jsx
@@ -1,6 +1,25 @@
 import DashboardIcon from "@mui/icons-material/Dashboard";
 import Box from "@mui/material/Box";
 import Chip from "@mui/material/Chip";
+import Avatar from "@mui/material/Avatar";
+import AvatarGroup from "@mui/material/AvatarGroup";
+import Tooltip from "@mui/material/Tooltip";
+import Button from "@mui/material/Button";
+import AddIcon from "@mui/icons-material/Add";
+
+const CHIP_STYLE = {
+  color: "primary.main",
+  paddingX: "5px",
+  borderRadius: "5px",
+  backgroundColor: "white",
+  "& .MuiSvgIcon-root": {
+    color: "primary.main",
+  },
+  "&:hover": {
+    backgroundColor: "primary.light",
+  },
+};
+
 function BoardBar() {
   return (
     <>
@@ -23,9 +42,44 @@ function BoardBar() {
             label="DashBoard"
             clickable
             onclick={() => {}}
+            sx={CHIP_STYLE}
+          />
+          <Chip
+            icon={<DashboardIcon />}
+            label="DashBoard"
+            clickable
+            onclick={() => {}}
+            sx={CHIP_STYLE}
           />
         </Box>
-        <Box sx={{ display: "flex", alignItems: "center", gap: 2 }}></Box>
+        <Box sx={{ display: "flex", alignItems: "center", gap: 2 }}>
+          <Button variant="outlined" startIcon={<AddIcon></AddIcon>}>
+            Invite
+          </Button>
+          <AvatarGroup
+            max={6}
+            total={24}
+            sx={{
+              "& .MuiAvatar-root": {
+                height: 34,
+                width: 34,
+              },
+            }}
+          >
+            <Tooltip title="Remy Sharp">
+              <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+            </Tooltip>
+            <Tooltip title="Remy Sharp">
+              <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+            </Tooltip>
+            <Tooltip title="Remy Sharp">
+              <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+            </Tooltip>
+            <Tooltip title="Remy Sharp">
+              <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+            </Tooltip>
+          </AvatarGroup>
+        </Box>
       </Box>
     </>
   );
